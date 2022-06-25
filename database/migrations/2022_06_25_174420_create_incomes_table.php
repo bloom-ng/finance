3da6a30e450b861b6617be387caed5354ff2b77e
emmymayo <odudareemmanuel@gmail.com>
2022-06-25 18:57:33 +0100

    models created - preparation pending

---
+++ database/migrations/2022_06_25_174420_create_incomes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncomesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('income_type_id')->constrained();
+            $table->foreignId('payer_id')->constrained();
+            $table->double('amount', 10, 2);
+            $table->string('payment_method');
+            $table->string('remark');
+            $table->date('payment_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incomes');
+    }
+}
