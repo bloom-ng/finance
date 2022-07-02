3ff7175a8d201d5d07b3234095cbfdebc099d8a3
bloom-ng <it@bloomdigitmedia.com>
2022-07-02 21:06:20 +0100

    Did the Login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,27 +1,78 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Finance App</title>
-</head>
-<body>
-    <form method="post" action="{{ route('login-user') }}">
-        @csrf
-      <div class="form-group">
-        <label>Email</label>
-        <input type="email" name="email" class="form-control p_input">
+  <head>
+  	<title>Finance | Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+
+	</head>
+	<body>
+    <section class="ftco-section">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6 text-center mb-5">
+            <h2 class="text heading-section">Login</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-7 col-lg-5">
+            <div class="wrap">
+                <div class="img" style="background-image: url(images/bg-1.jpg);"></div>
+                <div class="login-wrap p-4 p-md-5">
+                    <div class="d-flex">
+                        <div class="w-100">
+                            <h3 class="mb-4">Sign In</h3>
+                        </div>
+                        <div class="w-100">
+                            <p class="social-media d-flex justify-content-end">
+                                <a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
+                                <a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
+                            </p>
+                        </div>
+                    </div>
+                    <form method="post" action="{{ route('login-user') }}" class="signin-form">
+                        <div class="form-group mt-3">
+                            <input type="email" name="email" class="form-control" required>
+                            <label class="form-control-placeholder" for="email">Email</label>
+                        </div>
+                        <div class="form-group">
+                            <input id="password-field" type="password" name="password" class="form-control" required>
+                            <label class="form-control-placeholder" for="password">Password</label>
+                            <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
+                        </div>
+                        <div class="form-group d-md-flex">
+                            <div class="w-50 text-left">
+                                <label class="checkbox-wrap checkbox-primary mb-0">Remember Me
+                                    <input type="checkbox" checked>
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>
+                                <div class="w-50 text-md-right">
+                                    <a href="#">Forgot Password</a>
+                                </div>
+                            </div>
+                    </form>
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="form-group">
-        <label>Password *</label>
-        <input type="password" name="password" class="form-control p_input">
-      </div>
-    
-      <div class="text-center">
-        <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
-      </div>
-    
-    </form>
-</body>
+    </section>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+	</body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Finance</title>
 </head>
 <body>
-    Home 
-    <a href="{{route('login-user')}}">Login</a>
+    <a href="{{route('login-user')}}"><button>Login</button></a>
 </body>
 </html>
