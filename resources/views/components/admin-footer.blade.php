d05fc4852daeacca5134c4b9c9799daf81f7318a
David Aremu <segun8427@gmail.com>
2022-07-29 19:14:43 +0100

    Created Components & styled pages

---
+++ resources/views/components/admin-footer.blade.php
@@ -0,0 +1,23 @@
+</div>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+</script>
+<script src="/js/scripts.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+{{$slot}}
+<script>
+    document.getElementById("summaryModal").onclick = function summaryModal(){pop alert(<h1>Welcome</h1>)};
+    // function summaryModal(){
+    //     document.getElementById("summaryModal");
+    // }
+
+</script>
+
+</body>
+
+</html>
