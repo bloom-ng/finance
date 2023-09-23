a8494ee777d6f1b07bd9a15e29c7f0a5e50ec963
David Aremu <segun8427@gmail.com>
2023-09-23 23:19:34 +0100

    fixed typo in IncomeController.php

--- app/Http/Controllers/Admin/IncomeController.php
+++ app/Http/Controllers/Admin/IncomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
-use App\Http\controllers\Controller;
+use App\Http\Controllers\Controller;
 use App\Models\Income;
 use App\Models\IncomeType;
 use App\Models\PayerType;
